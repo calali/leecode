d1baa7de112fe1932b56d4268a62f7210f6aaeff
liqiuhui <cala_li@163.com>
2021-04-09 10:53:37 +0800

    feat: add execise

--- exercise/array/34.js
+++ exercise/array/34.js
@@ -27,23 +27,42 @@
 // 0 <= nums.length <= 105
 //   - 109 <= nums[i] <= 109
 // nums 是一个非递减数组
+// //   - 109 <= target <= 109
+// function binarySearch(arr,target) {
+//   const len = arr.length
+//   let left = 0
+//   let right = len - 1
+
+//   while (left <= right) {
+//     const middle = Math.round((left + right) / 2)
+//     if (arr[middle] > target) {
+//       right = middle - 1
+//     } else if (arr[middle] < target) {
+//       left = middle + 1
+//     } else {
+//       return middle
+//     }
+//   }
+//   return -1
+// }
 //   - 109 <= target <= 109
-function binarySearch(arr,target) {
+function binarySearch(arr,target,lower) {
   const len = arr.length
   let left = 0
   let right = len - 1
+  let ans = arr.length
 
   while (left <= right) {
-    const middle = Math.round((left + right) / 2)
-    if (arr[middle] > target) {
+    const middle = Math.floor((left + right) / 2)
+    if (arr[middle] > target || (lower && arr[middle] >= target)) {
       right = middle - 1
-    } else if (arr[middle] < target) {
-      left = middle + 1
+      ans = middle
     } else {
-      return middle
+      left = middle + 1
     }
   }
-  return -1
+
+  return ans
 }
 /**
  * @param {number[]} nums
@@ -51,29 +70,15 @@ function binarySearch(arr,target) {
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  const targetIndex =  binarySearch(nums,target)
-  if (targetIndex === -1){
-    return [-1,-1]
-  }else{
-    console.log(targetIndex);
-    let min = targetIndex
-    let max = targetIndex
-    // 查找左边界和右边界
-    for (let index = targetIndex-1; index >=0; index--) {
-      if (nums[index] === target){
-        min = index
-      }
-    }
-    for (let index = targetIndex+1,len = nums.length; index < len; index++) {
-      if (nums[index ] === target) {
-        max = index
-      }
-    }
-    return [min,max]
+  const leftIndex =  binarySearch(nums,target,true)
+  const rightIndex =  binarySearch(nums,target,false) -1
+  if(leftIndex>=0 && (rightIndex<=nums.length-1) && leftIndex <=rightIndex){
+    return [leftIndex,rightIndex]
   }
+  return [-1,-1]
 };
 // console.log(searchRange([2,2], 2));
 // console.log(searchRange([1], 1));
-console.log(searchRange([5, 7, 8, 8, 8, 10], 8));
-console.log(searchRange([5, 7, 7, 8, 8, 10], 0));
-console.log(searchRange([],0));
+// console.log(searchRange([5, 7, 8, 8, 8, 10], 8));
+// console.log(searchRange([5, 7, 7, 8, 8, 10], 0));
+// console.log(searchRange([],0));
